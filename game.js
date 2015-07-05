2a5b99392fc237e66fb5cab690257e90c0425144
Elliot Winkler <elliot.winkler@gmail.com>
2015-07-05 14:43:55 -0500

    Mark certain cells as mines
    Now let's add the thing which the whole game centers around: mines. We
    aren't doing anything fancy to place them: we simply choose three cells
    at random, and we set a class on them so that we can style them
    differently.

--- game.js
+++ game.js
@@ -1,14 +1,32 @@
-// Generate board
+// Set some globals
 
 var cellSize = 50;  // pixels
 var boardLength = 9;  // number of rows and cells
 var boardSize = (cellSize * boardLength);
+var totalNumberOfCells = boardLength * boardLength;
+var numberOfMines = 10;
 var board = $("#board");
+
+// Generate mines
+
+var mines = [];
+for (var i = 0; i < numberOfMines; i++) {
+  var mine = Math.floor(Math.random() * totalNumberOfCells);
+  mines.push(mine);
+}
+
+// Generate board
 
 for (var y = 0; y < boardLength; y++) {
   var row = $('<tr>');
   for (var x = 0; x < boardLength; x++) {
     var cell = $('<td>');
+
+    // Determine whether cell is a mine
+    if (mines.indexOf((y * boardLength) + x) !== -1) {
+      cell.addClass('mine');
+    }
+
     row.append(cell);
   }
   board.append(row);
