e767b6bbfe992e88b96ea92855606de5020296ad
Elliot Winkler <elliot.winkler@gmail.com>
2015-07-05 22:20:01 -0500

    Reveal mines when clicked
    Here we add a click handler to each cell. If the cell is a mine, then we
    reveal all of the cells (whether a mine or not) and end the game.
    Otherwise, we reveal just that cell. The cell gets a number indicating
    how many cells that are adjacent to that cell are mines.
    
    To visually identify whether a cell is a mine, we apply a CSS class. To
    be able to look up whether a cell is a mine, we add a key to the `data`.
    
    We also have to be able to access the X and Y coordinate of a certain
    cell, so we store it in `data`.
    
    Finally, we have to be able to look up the neighboring cells of a
    particular cell. This means we need to be able to look up cells by their
    X and Y coordinate, so we use CSS classes for this. But, notice how
    cumbersome it is to do this! We're essentially using the DOM as a lookup
    table, which it was never intended to do. Surely there's a better way?

--- game.js
+++ game.js
@@ -1,3 +1,42 @@
+function encounterCell(cell) {
+  // Don't reveal a cell that's already been revealed
+  if (!cell.hasClass('revealed')) {
+    cell.addClass('revealed');
+
+    if (!cell.data('isMine')) {
+      // The cell is near a mine
+
+      var x = cell.data('x');
+      var y = cell.data('y');
+
+      var adjacentCells = [
+        $('td.x-' + (x - 1) + '.y-' + (y - 1)),
+        $('td.x-' + x + '.y-' + (y - 1)),
+        $('td.x-' + (x + 1) + '.y-' + (y - 1)),
+        $('td.x-' + (x - 1) + '.y-' + y),
+        $('td.x-' + (x + 1) + '.y-' + y),
+        $('td.x-' + (x - 1) + '.y-' + (y + 1)),
+        $('td.x-' + x + '.y-' + (y + 1)),
+        $('td.x-' + (x + 1) + '.y-' + (y + 1))
+      ];
+
+      // Count mines that are in the vicinity,
+      // i.e., in a 3x3 radius around this cell
+
+      var numberOfAdjacentMines = 0;
+      for (var k = 0; k < adjacentCells.length; k++) {
+        if (adjacentCells[k].data('isMine')) {
+          numberOfAdjacentMines++;
+        }
+      }
+
+      if (numberOfAdjacentMines > 0) {
+        cell.append(numberOfAdjacentMines);
+      }
+    }
+  }
+}
+
 // Set some globals
 
 var cellSize = 50;  // pixels
@@ -22,10 +61,33 @@ for (var y = 0; y < boardLength; y++) {
   for (var x = 0; x < boardLength; x++) {
     var cell = $('<td>');
 
+    // Store the coordinates of the cell so we can find adjacent cells
+    cell.data('x', x);
+    cell.data('y', y);
+    cell.addClass('x-' + x);
+    cell.addClass('y-' + y);
+
     // Determine whether cell is a mine
     if (mines.indexOf((y * boardLength) + x) !== -1) {
       cell.addClass('mine');
+      cell.data('isMine', true);
     }
+
+    cell.on("click", function () {
+      var cell = $(this);
+
+      if (cell.data('isMine')) {
+        // Reveal everything
+        for (var y = 0; y < boardLength; y++) {
+          for (var x = 0; x < boardLength; x++) {
+            var cell = $('td.x-' + x + '.y-' + y);
+            encounterCell(cell);
+          }
+        }
+      } else {
+        encounterCell(cell);
+      }
+    });
 
     row.append(cell);
   }
